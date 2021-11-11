5b5a47dd575d6da7bf052cf2bbbc4d3c41a93a2d
Alexander <72731610+Alehandro-Montana@users.noreply.github.com>
2021-11-11 10:20:07 +0200

    Update widget.cpp

--- Gubanov_labs/lab6/widget.cpp
+++ Gubanov_labs/lab6/widget.cpp
@@ -44,41 +44,4 @@ void Widget :: moveEvent(QMoveEvent *e)
     edit1->setText(QString::number(e->pos().x()) + " widget x");
     edit2->setText(QString::number(e->pos().y()) + " widget y");
 }
-void Widget ::mouseMoveEvent(QMouseEvent *e)
-{
-    edit5->setText(QString::number(e->x()) + " mouse in widget x");
-    edit6->setText(QString::number(e->y()) + " mouse in widget y");
 
-    edit5->setText(QString::number(e->globalX()) + " mouse in global x");
-    edit6->setText(QString::number(e->globalY()) + " mouse in global Y");
-}
-
-void Widget::mousePressEvent(QMouseEvent *e){
-
-
-    bool leftClick = e->buttons() & Qt::LeftButton;
-    bool rightClick = e->buttons() & Qt::RightButton;
-
-    if(leftClick){
-        btL++;
-        lbl1->setNum(btL);
-    }
-    else if(rightClick){
-        btR++;
-        lbl2->setNum(btR);
-    }
-    //if(Qt::LeftButton&e->button())
-      //  lbl1->setNum(lbl1->(setValue)+1);
-}
-
-void Widget::keyPressEvent(QKeyEvent *e){
-    bool shiftPressed = e->modifiers() & Qt::ShiftModifier;
-    if(Qt::Key_L == e->key()){
-        btL = 0;
-        lbl1->setNum(btL);
-    }
-    else if(Qt::Key_R == e->key() && shiftPressed){
-        btR = 0;
-        lbl2->setNum(btR);
-    }
-}
