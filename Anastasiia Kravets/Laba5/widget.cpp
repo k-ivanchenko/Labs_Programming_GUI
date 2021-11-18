37d536306fe2f3c074b9c9d48c28b03400b12cd4
Anastasiia Kravets <73073087+AnastasiiaKravetz@users.noreply.github.com>
2021-11-18 13:31:54 +0200

    Add files via upload

---
+++ Anastasiia Kravets/Laba5/widget.cpp
@@ -0,0 +1,61 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+    Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    resize(400, 400);
+
+
+    lcd = new QLCDNumber(2, this);
+    list = new QListWidget(this);
+    slider = new QSlider(Qt::Vertical, this);
+    label = new QLabel("HELP",this);
+    button = new QPushButton("PRESS", this);
+
+    list->move(10, 100);
+    label->move(10, 30);
+    lcd->move(300, 200);
+    slider->move(300, 100);
+    button->move(10, 325);
+    slider->setTickPosition(QSlider::TicksBothSides);
+    slider->setRange(0, 99);
+
+
+
+    connect(slider, SIGNAL(valueChanged(int)), this, SLOT(LCDAD()));
+
+    connect(slider, SIGNAL(valueChanged(int)), this, SLOT(LC(int)));
+    connect(button, SIGNAL(clicked()), this, SLOT(SHOW()));
+
+}
+    void Widget::LCDAD() {
+
+
+        int random_value = rand() % 100;
+    list->addItem(QString::number(random_value));
+
+            };
+
+
+
+    void Widget::LC(int i) {
+
+       lcd->display(i);
+            }
+
+
+    void Widget::SHOW() {
+        for(int i=0; i<list->count();i++){
+        this->label->setText(list->item(i)->text());
+
+            }
+};
+
+
+Widget::~Widget()
+{
+    delete ui;
+}
---
+++ Anastasiia Kravets/Laba5/widget.h
@@ -0,0 +1,44 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QLCDNumber>
+#include <QListWidget>
+#include <QSlider>
+#include <QLabel>
+#include <QPushButton>
+#include <QListWidgetItem>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+public:
+    QLCDNumber *lcd;
+
+
+    QListWidget *list;
+    QSlider *slider;
+    QLabel *label;
+    QPushButton *button;
+
+
+public slots:
+void LCDAD();
+void LC(int i);
+void SHOW();
+
+
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+};
+#endif // WIDGET_H
